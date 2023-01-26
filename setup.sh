a70d16237f7e1c8dd66cade21aa77e939b257ec9
Cesar Claude <cesarclaudepro@gmail.com>
2023-01-26 16:51:46 +0000

    :sparkles: finished p2

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Install GNS3
 sudo apt update
-sudo apt install -y python3-pip python3-pyqt5 python3-pyqt5.qtsvg \
+sudo apt install -y -qq python3-pip python3-pyqt5 python3-pyqt5.qtsvg \
 python3-pyqt5.qtwebsockets \
 qemu qemu-kvm qemu-utils libvirt-clients libvirt-daemon-system virtinst \
 wireshark xtightvncviewer apt-transport-https \
@@ -11,21 +11,22 @@ sudo pip3 install gns3-server gns3-gui
 
 # Install Ubridge
 cd /tmp
-sudo apt install -y git build-essential pcaputils  libpcap-dev
+sudo apt install -y -qq git build-essential pcaputils  libpcap-dev
 git clone https://github.com/GNS3/ubridge.git
 cd ubridge
 sudo make install
 
 # Fix auxiliary console issue
-sudo apt intall -y xterm
+sudo apt intall -y -qq xterm
 
 # Fix Wireshark permissions issue
+sudo apt install -y -qq debconf-utils
 echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
 sudo dpkg-reconfigure wireshark-common -fnoninteractive
 
 # Install dynamips
 cd /tmp
-sudo apt install -y libelf-dev libpcap-dev cmake
+sudo apt install -y -qq libelf-dev libpcap-dev cmake
 git clone https://github.com/GNS3/dynamips.git
 cd dynamips
 mkdir build
