d3214af17fd079548839d7b2111cf58e8f6301ad
Cesar Claude <cesarclaudepro@gmail.com>
2023-01-30 23:34:17 +0000

    :art: refactored and prepared for turn-in

--- docker/router/dynamic_vxlan.sh
+++ docker/router/dynamic_vxlan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# A script to set up the VXLAN of the routers in P2
+# A script to set up the dynamic VXLAN of the routers in P2
 
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
@@ -8,10 +8,8 @@ fi
 
 if [ $1 = "1" ]; then
     LOCAL=1
-    REMOTE=2
 elif [ $1 = "2" ]; then
     LOCAL=2
-    REMOTE=1
 else
     exit
 fi
@@ -27,7 +25,7 @@ ip link set dev br0 up
 ip addr add 10.1.1.$LOCAL/24 dev eth0
 
 # Create VXLAN
-ip link add name vxlan10 type vxlan id 10 dev eth0 local 10.1.1.$LOCAL remote 10.1.1.$REMOTE dstport 4789
+ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
 ip addr add 20.1.1.$LOCAL/24 dev vxlan10
 
 # Add to bridge
